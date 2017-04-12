b31e7b4687fd0bbdba7148462b820f680450dfb9
fafaschiavo <fayschiavo@gmail.com>
2017-04-13 01:18:34 +0200

    tor tests added

---
+++ tor_tests/ip_generator.py
@@ -0,0 +1,16 @@
+import urllib2
+from TorCtl import TorCtl
+
+proxy_support = urllib2.ProxyHandler({"http" : "127.0.0.1:8118"})
+opener = urllib2.build_opener(proxy_support) 
+
+def newId():
+    conn = TorCtl.connect(controlAddr="127.0.0.1", controlPort=9051, passphrase="your_password")
+    conn.send_signal("NEWNYM")
+
+for i in range(0, 10):
+    print "case "+str(i+1)
+    newId()
+    proxy_support = urllib2.ProxyHandler({"http" : "127.0.0.1:8118"})
+    urllib2.install_opener(opener)
+    print(urllib2.urlopen("http://www.ifconfig.me/ip").read())
---
+++ tor_tests/tor_test.py
@@ -0,0 +1,30 @@
+import socks
+import socket
+import requests
+import os
+import time
+
+def reset_ip():
+	try:
+		os.system("/etc/init.d/tor restart")
+		time.sleep(1)
+	except:
+		pass
+
+	socks.setdefaultproxy(proxy_type=socks.PROXY_TYPE_SOCKS5, addr="127.0.0.1", port=9050)
+	socket.socket = socks.socksocket
+	return None
+
+
+
+
+# Here comes the main function
+reset_ip()
+print requests.get("http://icanhazip.com").text
+
+
+
+# 81.57.131.168 - meu IP
+# 51.15.63.229
+# 119.81.135.2
+# 51.15.62.146
